a2e97fbc939a3ca4b440519b9378f302794a15c6
Unknownmaster0 <sagar192002singh@gmail.com>
2026-01-09 14:50:17 +0530

    intialize the spring boot project

---
+++ fancysticker-server/src/test/java/org/example/fancystickerserver/FancystickerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fancystickerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancystickerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
