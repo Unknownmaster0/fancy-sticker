7e59aececab7cb811eef5793786e94e3ebf1f67b
Unknownmaster0 <sagar192002singh@gmail.com>
2026-01-10 23:24:21 +0530

    feat: h2 database configuration and spring JPA

--- fancysticker-server/src/main/java/org/example/fancystickerserver/FancystickerServerApplication.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/FancystickerServerApplication.java
@@ -2,8 +2,10 @@ package org.example.fancystickerserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"org.example.fancystickerserver.controller"})
 public class FancystickerServerApplication {
 
     public static void main(String[] args) {
