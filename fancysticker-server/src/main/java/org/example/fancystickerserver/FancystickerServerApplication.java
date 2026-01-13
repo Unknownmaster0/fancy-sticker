f59ca6792b1404d53f14616706ad3941b5e1c680
Unknownmaster0 <sagar192002singh@gmail.com>
2026-01-13 18:41:51 +0530

    cors configuration

--- fancysticker-server/src/main/java/org/example/fancystickerserver/FancystickerServerApplication.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/FancystickerServerApplication.java
@@ -2,7 +2,6 @@ package org.example.fancystickerserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {"org.example.fancystickerserver.controller"})
--- fancysticker-server/src/main/java/org/example/fancystickerserver/controller/ProductController.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/controller/ProductController.java
@@ -3,6 +3,7 @@ package org.example.fancystickerserver.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.fancystickerserver.dto.ProductDto;
 import org.example.fancystickerserver.services.IProductService;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+//@CrossOrigin(origins = {"http://localhost:5173"})
 public class ProductController {
 
     private final IProductService productService;
