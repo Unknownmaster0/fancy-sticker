b226a3ffc7c456f96fbb5db15a4072edc41802ef
Unknownmaster0 <sagar192002singh@gmail.com>
2026-03-19 17:10:34 +0530

    feat: Authority/Role functionality in Application

--- fancysticker-server/src/main/java/org/example/fancystickerserver/entity/Role.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/entity/Role.java
@@ -1,31 +1,25 @@
 package org.example.fancystickerserver.entity;
 
-@lombok.Getter
-@lombok.Setter@jakarta.persistence.Entity
-@jakarta.persistence.Table(name = "roles")
-@jakarta.persistence.AttributeOverrides({
-@jakarta.persistence.AttributeOverride(name = "createdBy",
-column = @jakarta.persistence.Column(nullable = false,
-length = 20)),
-@jakarta.persistence.AttributeOverride(name = "updatedBy",
-column = @jakarta.persistence.Column(length = 20))})
-public class Role extends org.example.fancystickerserver.entity.BaseEntity {
-@jakarta.persistence.Id
-@jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-@jakarta.persistence.Column(name = "role_id", nullable = false)
-private java.lang.Long id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-@jakarta.validation.constraints.NotNull
-@jakarta.persistence.ManyToOne(fetch = jakarta.persistence.FetchType.LAZY, optional = false)
-@org.hibernate.annotations.OnDelete(action = org.hibernate.annotations.OnDeleteAction.CASCADE)
-@jakarta.persistence.JoinColumn(name = "customer_id", nullable = false)
-private org.example.fancystickerserver.entity.Customer customer;
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+    private Long id;
 
-@jakarta.validation.constraints.Size(max = 50)
-@jakarta.validation.constraints.NotNull
-@jakarta.persistence.Column(name = "name", nullable = false, length = 50)
-private java.lang.String name;
-
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
 
 
 }
--- fancysticker-server/src/main/java/org/example/fancystickerserver/security/PublicPathConfig.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/security/PublicPathConfig.java
@@ -11,6 +11,8 @@ public class PublicPathConfig {
     public List<String> publicPaths() {
         return List.of("/api/v1/auth/**",
                 "/api/v1/products/**",
-                "/api/v1/contacts/**");
+                "/api/v1/contacts/**",
+                "/error" // this is needed to allow Spring Security to handle errors properly without causing infinite loops
+        );
     }
 }
