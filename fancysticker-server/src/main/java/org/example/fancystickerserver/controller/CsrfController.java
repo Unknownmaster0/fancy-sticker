f0f2716a1995625880d0073ae41be8f0f459d1bc
Unknownmaster0 <sagar192002singh@gmail.com>
2026-03-21 15:20:12 +0530

    feat: csrf implementation

--- fancysticker-server/src/main/java/org/example/fancystickerserver/controller/CsrfController.java
+++ fancysticker-server/src/main/java/org/example/fancystickerserver/controller/CsrfController.java
@@ -1,4 +1,18 @@
 package org.example.fancystickerserver.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/csrf-token")
 public class CsrfController {
+    @GetMapping
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        // in the security config, we set the CsrfTokenRequestAttributeHandler to save the token in the request
+        // attribute with the key CsrfToken.class.getName(). Read the details of the CsrfTokenRequestAttributeHandler in the Spring Security documentation.
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
 }
